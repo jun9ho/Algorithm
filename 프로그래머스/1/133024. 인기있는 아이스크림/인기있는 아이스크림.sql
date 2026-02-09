2d113d2518f8c330e1cdbe449ec15b3e185d1c0e
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-02-09 13:09:08 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+-- FIRST_HALF 테이블: 아이스크림 가게의 상반기 주문 정보를 담은 테이블
+-- 아이스크림의 맛을 총주문량을 기준으로 내림차순 정렬
+-- 총주문량이 같다면 출하 번호를 기준으로 오름차순 정렬하여 조회
+SELECT FLAVOR FROM FIRST_HALF ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID ASC
