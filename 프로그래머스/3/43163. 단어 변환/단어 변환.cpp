a3ea04759a0ada97c02c1924acf6a03ee8c9da93
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-02-14 11:14:40 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int bfs(int targetIndex,vector<int> v[],int n){
+    
+    queue<pair<int,int>> q; // index, dist
+    q.push({0,0});
+    vector<bool> vis(n,false);
+    
+    while(!q.empty()){
+        int curIndex = q.front().first;
+        int curDist = q.front().second;
+        q.pop();
+        if(curIndex==targetIndex){
+            return curDist;
+        }
+        for(int i=0;i<v[curIndex].size();i++){
+            if(vis[curIndex]) continue;
+            q.push({v[curIndex][i],curDist+1});
+        }
+        
+    }
+    
+    
+    
+}
+
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+    
+    auto iter = find(words.begin(),words.end(),target);
+    if(iter==words.end() ) return 0;
+    words.insert(words.begin(),begin);
+    vector<int> v[words.size()];
+    
+    for(int i=0;i<words.size();i++){
+        for(int j=0;j<words.size();j++){
+            if(words[i]==words[j]) continue;
+            int cnt =0;
+            for(int k=0;k<words[i].length();k++){
+                if(words[i][k]==words[j][k]) cnt++;
+            }
+            if(cnt == words[i].length()-1){
+                v[i].push_back(j);
+            }
+        }
+    }
+    
+    auto iter2 = find(words.begin(),words.end(),target);
+    return bfs(iter2-words.begin(),v,words.size());
+    
+    
+    
+    
+    return answer;
+}
