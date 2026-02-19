96b264fe984fb3422764cd519f99e8531d639263
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-02-19 12:56:43 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,11 +1,10 @@
--- 코드를 입력하세요
 -- 식품분류별로 가격이 제일 비싼 식품의 분류, 가격, 이름을 조회
--- 맛있는배추김치 19000 	맛있는김치찌개 2900 	맛있는마조유 8950 맛있는허니버터칩 	1950
 SELECT CATEGORY,PRICE AS MAX_PRICE,PRODUCT_NAME
 FROM FOOD_PRODUCT
 WHERE (CATEGORY,PRICE) IN(
     SELECT CATEGORY,MAX(PRICE)
     FROM FOOD_PRODUCT
-    WHERE CATEGORY IN ( '과자', '국', '김치', '식용유')
-    GROUP BY CATEGORY)
-ORDER BY MAX_PRICE DESC
+    WHERE CATEGORY IN( '과자', '국', '김치', '식용유')
+    GROUP BY CATEGORY
+)
+ORDER BY PRICE DESC
