32894acdda48b9777b15b53ac1ea7f32aa797593
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-03-27 10:43:55 +0900

    [level 2] Title: 튜플, Time: 15.90 ms, Memory: 6.08 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -2,31 +2,30 @@
 
 using namespace std;
 
-bool comp(pair<int,int> a,pair<int,int>b){
+bool comp(pair<int,int> a, pair<int,int>b){
     
     return a.second>b.second;
 }
 
 vector<int> solution(string s) {
     vector<int> answer;
-    map<int,int> m;
-    for (char& c : s){
-        if (string(",}{").find(c) != string::npos)
-            c = ',';
+    string regex="{},";
+    for(char& c : s){
+        if(regex.find(c)!=string::npos){
+            c=',';
+        }
     }
     stringstream ss(s);
     string token;
-    while (getline(ss, token, ',')) {
-        if(token.length()!=0) m[stoi(token)]++;
+    map<int,int> m;
+    while(getline(ss,token,',')){
+        if(token.size()==0) continue;
+        m[stoi(token)]++;
     }
     vector<pair<int,int>> v(m.begin(),m.end());
     sort(v.begin(),v.end(),comp);
-    
-    
-    //, 구분으로 숫자 파싱해서 가져오고 -> 이건 string이기에 stoi해가지고 map으로가져오고
     for(int i=0;i<v.size();i++){
         answer.push_back(v[i].first);
     }
-    
     return answer;
 }
