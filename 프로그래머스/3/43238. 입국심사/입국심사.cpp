e1af3bdedf249d4e23e78bf9721766e20d2e8e20
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-03-28 10:22:15 +0900

    [level 3] Title: 입국심사, Time: 37.95 ms, Memory: 6.42 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -2,40 +2,25 @@
 
 using namespace std;
 
-bool doit(long long mid,vector<int> times,int n){
-    long long sum=0;
-    for(int i=0;i<times.size();i++){
-        sum +=  (mid/times[i]);
-    }
-    if(sum>=n) return true;
-    else return false;
-    
-}
-
 long long solution(int n, vector<int> times) {
     long long answer = 0;
-    
-    sort(times.begin(),times.end());
-    
-    long long r = 0;
-    long long l = n*times[times.size()-1];
-    
-    while(r<l){
-        long long mid = (r+l)/2;
-        
-        bool isCan = doit(mid,times,n);
-        if(isCan){
-            l = mid;
+
+    long long maxt = 1000000000000000;
+    long long mint = 0;
+    while(mint<maxt){
+        long long curt = (mint+maxt)/2;
+        long long sumn = 0;
+        for(int time: times){
+            sumn+=(curt/time); 
+        }
+        if(sumn>=n){
+            maxt = curt;
         }
         else{
-            r = mid+1;
+            mint = curt+1;
         }
-        if(r==l)
-            break;
-        
     }
     
     
-    answer = r;
-    return answer;
+    return mint;
 }
