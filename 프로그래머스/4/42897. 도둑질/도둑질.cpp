34b098c4e8222c71611239016a21a3f2f8cf192c
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-03-28 11:25:06 +0900

    [level 4] Title: 도둑질, Time: 7.38 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-long long solution(vector<int> money) {
-    long long answer = 0;
-    if(money.size()==3){
-        return max(max(money[0],money[1]),money[2]);
-    }
-    vector<long long> v1(money.begin(),money.end()-1);
-    vector<long long> v2(money.begin()+1,money.end());
-    vector<long long> dp1(v1.size(),0);
-    vector<long long> dp2(v2.size(),0);
-    dp1[0] = v1[0];
-    dp1[1] = max(v1[0],v1[1]);
-    dp2[0] = v2[0];
-    dp2[1] = max(v2[0],v2[1]);
-    for(int i=2;i<v1.size();i++){
-        dp1[i] = max(dp1[i-1],dp1[i-2]+v1[i]);
-    }
-    for(int i=2;i<v1.size();i++){
-        dp2[i] = max(dp2[i-1],dp2[i-2]+v2[i]);
+int solution(vector<int> money) {
+    int answer = 0;
+    int size = money.size();
+    vector<int> dp1(size-1,0);// 0 ~ size-1
+    vector<int> dp2(size-1,0);// 1 ~ size
+    
+    dp1[0] = money[0];
+    dp1[1] = max(money[0],money[1]);
+    for(int i=2;i<dp1.size();i++){
+        dp1[i] = max(money[i]+dp1[i-2],dp1[i-1]);
     }
     
-    return max(dp1[dp1.size()-1],dp2[dp2.size()-1]);
+    dp2[0] = money[1];
+    dp2[1] = max(money[1],money[2]);
+    
+    for(int i=2;i<dp2.size();i++){
+        dp2[i] = max(money[i+1]+dp2[i-2],dp2[i-1]);
+    }
+    
+    
+    return max(dp1[dp1.size()-1],dp2[dp1.size()-1]);
 }
