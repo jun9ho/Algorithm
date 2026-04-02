5dd4c0fb2115c62091b078369a1b2354b49b7f5a
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-02 09:40:58 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -2,28 +2,29 @@
 
 using namespace std;
 
-// A0 B1 C2 D3 E4 F5 G6 H7 I8 J9 K10 L11 M12 N13 O12 P11 Q10 R9 S8 T7 U6 V5 W4 X3 Y2 Z1
-//
-
 int solution(string name) {
     int answer = 0;
-    unordered_map<char,int> m;
-    for(int i=0;i<13;i++){
-        m['A'+i]=i;
-        m['Z'-i]=i+1;
+    int size = name.size();
+    
+    //A0 B1 C2 D3 E4 F5 G6 H7 I8 J9 K10 L11 M12 N13 O12 P11 Q10 R9 S8 T7 U6 V5 W4 X3 Y2 Z1
+    map<char,int> m;
+    for(int i=0;i<26;i++){
+        m['A'+i]=min(i,26-i);
     }
-    for(int i=0; i<name.length();i++){
+    for(int i=0;i<name.size();i++){
         answer+=m[name[i]];
     }
-    int n = name.length();
-    //"DASDLKAAAAAAABAAAAAAAASDDSS"
-    int move = n - 1;
-    for (int i=0; i <n;i++) {
-        int j = i+1;
-        while (j<n && name[j] == 'A') j++;
-        move = min(move, 2 * i + (n - j));
-        move = min(move, i + 2 * (n - j));
+    if(answer==0) return 0;
+    if(size==1){
+        return answer;
     }
-    answer+=move;
-    return answer;
+    int k = size-1;
+    for(int i=0;i<size;i++){
+        int j = i+1;
+        while(j<size && name[j]=='A')j++;
+        int a = i*2 + size-j;
+        int b = i + (size-j)*2;
+        k = min(k,min(a,b));
+    }
+    return answer+k;
 }
