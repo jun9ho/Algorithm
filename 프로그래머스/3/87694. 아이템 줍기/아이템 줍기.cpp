4179a991240b562e7c5798fc76c0f86b081a978e
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-03 11:37:44 +0900

    [level 3] Title: 아이템 줍기, Time: 0.13 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -2,62 +2,73 @@
 
 using namespace std;
 
-int dx[4]= {1,-1,0,0};
-int dy[4]= {0,0,1,-1};
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
 
-int bfs(int curx, int cury, int endx, int endy,vector<vector<int>> v){
-    
+
+int bfs(vector<vector<int>> v,int characterX, int characterY, int itemX, int itemY ){
     queue<pair<pair<int,int>,int>> q;
-    vector<vector<int>> dist(102,vector<int>(102,10000));
-    q.push({{curx,cury},0});
-    dist[curx][cury]=0;
-    
+    q.push({{characterX,characterY},0});
+    vector<vector<int>> dist(v.size(),vector<int>(v[0].size(),INT_MAX));
+    dist[characterX][characterY] =0;
     while(!q.empty()){
-        int cx = q.front().first.first;
-        int cy = q.front().first.second;
-        int cd = q.front().second;
+        int curx = q.front().first.first;
+        int cury = q.front().first.second;
+        int curd = q.front().second;
         q.pop();
-        if(cx==endx && cy==endy){
-            return cd;
+        if(curx==itemX && cury == itemY){
+            return curd;
         }
         for(int i=0;i<4;i++){
-            int nx = cx+dx[i];
-            int ny = cy+dy[i];
-            int nd = cd+1;
-            if((nx<=1)||(ny<=1)||(nx>=101)||(nx>=101)) continue;
-            if(!v[nx][ny]) continue;
-            if(dist[nx][ny]<=nd) continue;
-            dist[nx][ny]=nd;
+            int nx = curx+dx[i];
+            int ny = cury+dy[i];
+            int nd = curd+1;
+            if(nx>100||ny>100||nx<1||ny<1) continue;
+            if(nd>=dist[nx][ny]) continue;
+            if(v[nx][ny]==0) continue;
             q.push({{nx,ny},nd});
+            dist[nx][ny]=nd;
         }
-        
     }
-    
-    
-    
-    
-    return -1;
+    return 0;
 }
-
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
     int answer = 0;
+    vector<vector<int>> v(101,vector<int>(101,0));
+    for(int i=0;i<rectangle.size();i++){
+        int ldx = rectangle[i][0];
+        int ldy = rectangle[i][1];
+        int rux = rectangle[i][2];
+        int ruy = rectangle[i][3];
+        ldx*=2;
+        ldy*=2;
+        rux*=2;
+        ruy*=2;
+        for(int j=ldx;j<=rux;j++){
+            for(int k=ldy;k<=ruy;k++){
+                v[j][k]=1;
+            }
+        }
+    }
+    for(int i=0;i<rectangle.size();i++){
+        int ldx = rectangle[i][0];
+        int ldy = rectangle[i][1];
+        int rux = rectangle[i][2];
+        int ruy = rectangle[i][3];
+        ldx*=2;
+        ldy*=2;
+        rux*=2;
+        ruy*=2;
+        for(int j=ldx+1;j<rux;j++){
+            for(int k=ldy+1;k<ruy;k++){
+                v[j][k]=0;
+            }
+        }
+    }
     
-    vector<vector<int>> v(102,vector<int>(102,0));
-    for(int i=0;i<rectangle.size();i++){
-        for(int a=rectangle[i][0]*2;a<=rectangle[i][2]*2;a++){
-            for(int b=rectangle[i][1]*2;b<=rectangle[i][3]*2;b++){
-                v[a][b]=1;
-            }
-        }
-    }
-    for(int i=0;i<rectangle.size();i++){
-        for(int a=rectangle[i][0]*2+1;a<rectangle[i][2]*2;a++){
-            for(int b=rectangle[i][1]*2+1;b<rectangle[i][3]*2;b++){
-                v[a][b]=0;
-            }
-        }
-    }
-    answer = bfs(characterX*2, characterY*2, itemX*2, itemY*2, v);
+    answer = bfs(v,characterX*2,characterY*2,itemX*2,itemY*2);
+    
+    
     
     return answer/2;
 }
