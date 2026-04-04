3adba11ea828cec1fd136cb53290ae1d68828f0d
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-04 14:30:02 +0900

    [level 2] Title: 타겟 넘버, Time: 3.17 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -2,24 +2,23 @@
 
 using namespace std;
 
-int cnt =0;
-void dfs(int idx, vector<int>& numbers, int& target,int answer){
+int answer = 0;
+void dfs(int idx,int sum, vector<int>& numbers, int target){
     
-    if(idx ==numbers.size()){
-        if(answer==target) cnt++;
+    if(idx==(numbers.size()-1)){
+        if(sum==target) answer++;
         return;
     }
     
-    dfs(idx+1,numbers,target,answer+numbers[idx]);
-    dfs(idx+1,numbers,target,answer-numbers[idx]);
+    dfs(idx+1,sum+numbers[idx],numbers,target);
+    dfs(idx+1,sum-numbers[idx],numbers,target);
+    
     return;
 }
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    dfs(0,numbers,target,0);
-    answer=cnt;
-    
+    numbers.push_back(0);
+    dfs(0,0, numbers,target);
     
     return answer;
 }
