5c790fa805adf66cd6a093ad22633d15eaf72668
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-07 21:48:28 +0900

    [level 1] Title: 이름이 없는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
+++ 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
@@ -1,5 +1,6 @@
--- 코드를 입력하세요
+-- 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성
+-- ID는 오름차순 정렬
 SELECT ANIMAL_ID
 FROM ANIMAL_INS
 WHERE NAME IS NULL
-ORDER BY ANIMAL_ID ASC
+ORDER BY ANIMAL_ID
