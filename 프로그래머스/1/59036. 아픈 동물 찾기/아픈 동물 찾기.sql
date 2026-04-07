40119ffa98d285f9b782edd4d465ad2bbad2fd65
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-07 21:45:51 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,6 @@
--- 코드를 입력하세요
+-- 아픈 동물의 아이디와 이름을 조회하는 SQL 문을 작성
+-- 아이디 순으로 조회 
 SELECT ANIMAL_ID,NAME
 FROM ANIMAL_INS
-WHERE INTAKE_CONDITION = 'Sick'
+WHERE INTAKE_CONDITION ='Sick'
 ORDER BY ANIMAL_ID
