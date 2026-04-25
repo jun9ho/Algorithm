a47bc559ce232de5588b707a43e93cef09e2333e
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-25 11:00:38 +0900

    [level 3] Title: 이중우선순위큐, Time: 818.08 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -4,48 +4,30 @@ using namespace std;
 
 vector<int> solution(vector<string> operations) {
     vector<int> answer;
-    deque<int> v;
-    int size = 0;
+    deque<int> dq;
+    
     for(int i=0;i<operations.size();i++){
-        stringstream ss(operations[i]);
-        string token;
-        int a=-1,b=-1;
-        while(getline(ss,token,' ')){
-            if(token=="I"||token=="D"){
-                if(token=="I"){
-                    a=0;
-                }
-                else if(token=="D"){
-                    a=1;
-                }
-            }
-            else{
-                b=stoi(token);
-            }
-        }
-        if(a==0){
-            v.push_back(b);
-            sort(v.begin(),v.end(),greater<>());
+        string s = operations[i];
+        stringstream ss(s);
+        string a,b;
+        ss>>a>>b;
+        if(a=="I"){
+            int k = stoi(b);
+            dq.push_back(k);
+            sort(dq.begin(),dq.end());
         }
         else{
-            if(v.size()==0){
-                continue;
+            if(dq.size()==0) continue;
+            if(b=="1"){
+                dq.pop_back();
             }
-            if(b==1){
-                v.pop_front();
+            else{
+                dq.pop_front();
             }
-            else if(b==-1){
-                v.pop_back();
-            }
+            
         }
     }
-    if(v.size()==0){
-        answer.push_back(0);
-        answer.push_back(0);
-    }
-    else{
-        answer.push_back(v.front());
-        answer.push_back(v.back());
-    }
-    return answer;
+    if(dq.size()==0) return {0,0};
+    
+    return {dq[dq.size()-1],dq[0]};
 }
