af1eba1f3b0ae77d04e3263b020d469804591b4a
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-27 10:38:20 +0900

    [level 3] Title: 숫자 게임, Time: 11.96 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int solution(vector<int> A, vector<int> B) {
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
-    //1 3 5 7
-    //2 2 6 8
-
-    //4 2 2 1
-    int cnt = 0;
-    int idx = 0;
-
-    for(int i = 0; i < A.size(); i++){
-        while(idx < B.size() && B[idx] <= A[i]){
-            idx++;
+    int answer = 0;
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    int size=0;
+    for(int i=0;i<B.size();i++){
+        if(B[i]>A[size]){
+            size++;
+            answer++;
         }
-        if (idx >= B.size()) return cnt;
-        cnt++;
-        idx++;
     }
-    return cnt;
+    return answer;
 }
