9fe93cd9f03536843da1f15e0b839436a44d471d
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-27 10:27:45 +0900

    [level 3] Title: 등굣길, Time: 0.10 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -2,25 +2,31 @@
 
 using namespace std;
 
+
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
-    vector<vector<long long>> v(m+2,vector<long long>(n+2,0));
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-            v[i][j]=1;
-        }
-    }
+    vector<vector<int>> dp(m,vector<int>(n,0));
+    vector<vector<int>> block(m,vector<int>(n,0));
+    
     for(int i=0;i<puddles.size();i++){
-        v[puddles[i][0]][puddles[i][1]] = 0;
+        dp[--puddles[i][0]][--puddles[i][1]] =0;
+        block[puddles[i][0]][puddles[i][1]] =1;
     }
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-            if((i==1)&&(j==1)) continue;
-            if(v[i][j]==0) continue;
-            v[i][j] = v[i-1][j] % 1000000007 + v[i][j-1] % 1000000007;
-            v[i][j] %= 1000000007;
+    
+    dp[0][0]=1;
+    
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0&&j==0) continue;
+            if(block[i][j]) continue;
+            if(i==0) dp[i][j]=dp[i][j-1];
+            else if(j==0) dp[i][j]=dp[i-1][j];
+            else{
+                dp[i][j]=dp[i-1][j]%1000000007+dp[i][j-1]%1000000007;
+                dp[i][j] %=1000000007;    
+            }
         }
     }
     
-    return v[m][n];
+    return dp[m-1][n-1];
 }
