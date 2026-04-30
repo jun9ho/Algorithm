30f63293a3cfc19a64fa5804e737b1026cfb8d06
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-04-30 12:14:03 +0900

    [level 3] Title: 징검다리 건너기, Time: 79.93 ms, Memory: 14.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> stones, int k) {
+    int answer = INT_MAX;
+    multiset<int> ms;
+
+    for (int i = 0; i < k; i++) {
+        ms.insert(stones[i]);
+    }
+    answer = min(answer, *ms.rbegin());
+
+    for (int i = k; i < stones.size(); i++) {
+        ms.erase(ms.find(stones[i - k]));
+        ms.insert(stones[i]);             
+        answer = min(answer, *ms.rbegin());
+    }
+
+    return answer;
+}
