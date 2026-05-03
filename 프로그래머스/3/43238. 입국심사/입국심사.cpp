75426d364cd6ca09fbcb86af641064ac1dd00b98
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-04 07:57:10 +0900

    [level 3] Title: 입국심사, Time: 45.82 ms, Memory: 7.18 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -2,25 +2,35 @@
 
 using namespace std;
 
+bool isBig(long long c, int n, vector<int>times){
+    long long sum = 0;
+    for(auto time:times){
+        sum+=(c/time);
+    }
+    
+    if(sum>=n) return true;
+    else return false;
+}
+
 long long solution(int n, vector<int> times) {
     long long answer = 0;
-
-    long long maxt = 1000000000000000;
-    long long mint = 0;
-    while(mint<maxt){
-        long long curt = (mint+maxt)/2;
-        long long sumn = 0;
-        for(int time: times){
-            sumn+=(curt/time); 
-        }
-        if(sumn>=n){
-            maxt = curt;
+    
+    int num = times.size();//심사위원 수
+    sort(times.begin(),times.end());
+    long long max_time = 1000000000000000;
+    long long min_time = 0;
+    
+    while(min_time<max_time){
+        long long c = (max_time+min_time)/2;
+        bool big = isBig(c,n,times);
+        if(big){
+            max_time=c;
         }
         else{
-            mint = curt+1;
+            min_time=c+1;
         }
     }
     
     
-    return mint;
+    return min_time;
 }
