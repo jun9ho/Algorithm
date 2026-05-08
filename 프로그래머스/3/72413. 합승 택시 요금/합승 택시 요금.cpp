5fc60d4233032fd72e6e4c68a69b11792d6e351e
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-08 10:57:47 +0900

    [level 3] Title: 합승 택시 요금, Time: 2.12 ms, Memory: 7.86 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -3,29 +3,30 @@
 using namespace std;
 
 
-vector<int> bfs(int start, vector<vector<pair<int,int>>> v,int n){
-    queue<pair<int,int>> q;
-    vector<int> d(n,99999999);
+vector<int> bfs(vector<vector<pair<int,int>>> v,int n, int s){
     
-    q.push({start-1,0});
-    d[start-1]=0;
+    queue<pair<int,int>> q; //node, dist
+    vector<int> dist(n,INT_MAX);
+    
+    q.push({s,0});
+    dist[s]=0;
+    
     while(!q.empty()){
         int cur = q.front().first;
-        int curDist = q.front().second;
+        int curd = q.front().second;
         q.pop();
         for(int i=0;i<v[cur].size();i++){
             int next = v[cur][i].first;
-            int dist = v[cur][i].second;
-            if(dist+curDist>=d[next]) continue;
-            q.push({next,curDist+dist});
-            d[next] = curDist+dist;
-        }
-        
+            int nd = curd + v[cur][i].second;
+            if(nd>=dist[next]) continue;
+            q.push({next,nd});
+            dist[next]= nd;
+        }    
     }
     
-    return d;
+    
+    return dist;
 }
-
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     int answer = 0;
@@ -35,16 +36,21 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
         v[fares[i][1]-1].push_back({fares[i][0]-1,fares[i][2]});
     }
     
-    vector<int> ms = bfs(s,v,n);
-    vector<int> ma = bfs(a,v,n);
-    vector<int> mb = bfs(b,v,n);
-    vector<int> m(n,-1);
-    int sum = 99999999;
+    vector<int> ds,da,db;
+    s--;a--;b--;
+    ds = bfs(v,n,s);
+    da = bfs(v,n,a);
+    db = bfs(v,n,b);
+    int dA =0;
+    answer = INT_MAX;
     for(int i=0;i<n;i++){
-        if(ms[i]==99999999 || ma[i]==99999999 || mb[i]==99999999) continue;
-        m[i] = ms[i] + ma[i] + mb[i];
-        sum = min(sum,m[i]);
+        if(ds[i]==INT_MAX || da[i]==INT_MAX || db[i]==INT_MAX ) continue;
+        dA = ds[i]+da[i]+db[i];
+        if(dA<answer){
+            answer=dA;
+        }
     }
     
-    return sum;
+    
+    return answer;
 }
