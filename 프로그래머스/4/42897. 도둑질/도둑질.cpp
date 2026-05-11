f45311ce5066a473d6df4267e2569e33fcba6e5a
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-11 13:24:13 +0900

    [level 4] Title: 도둑질, Time: 5.05 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int solution(vector<int> money) {
     int answer = 0;
-    int size = money.size();
-    vector<int> dp1(size-1,0);// 0 ~ size-1
-    vector<int> dp2(size-1,0);// 1 ~ size
-    
+    vector<int> dp1(money.size()-1,0);
+    vector<int> dp2(money.size()-1,0);
+
     dp1[0] = money[0];
     dp1[1] = max(money[0],money[1]);
-    for(int i=2;i<dp1.size();i++){
-        dp1[i] = max(money[i]+dp1[i-2],dp1[i-1]);
-    }
     
     dp2[0] = money[1];
     dp2[1] = max(money[1],money[2]);
+    //1,2,3
+    //2,3,1
     
-    for(int i=2;i<dp2.size();i++){
-        dp2[i] = max(money[i+1]+dp2[i-2],dp2[i-1]);
+    for(int i=2;i<money.size()-1;i++){
+        dp1[i] = max(dp1[i-2]+money[i],dp1[i-1]);
+        
+        dp2[i] = max(dp2[i-2]+money[i+1],dp2[i-1]);
     }
     
     
-    return max(dp1[dp1.size()-1],dp2[dp1.size()-1]);
+    return max(dp1[money.size()-2],dp2[money.size()-2]);
 }
