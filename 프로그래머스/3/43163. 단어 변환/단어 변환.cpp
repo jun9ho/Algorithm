779e65037734d11922dbe393301f3b18bd1e39aa
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-11 12:15:11 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -3,67 +3,68 @@
 using namespace std;
 
 
-bool matching(string a, string b){
+bool match(string a,string b){
     if(a.size()!=b.size()) return false;
-    int cnt = 0;
-    for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i]) cnt++;
+    int cnt =0;
+    int len = a.size();
+    for(int i=0;i<len;i++){
+        if(a[i]==b[i]) cnt++;
     }
-    if(cnt==1) return true;
+    if(cnt==len-1) return true;
     else return false;
 }
 
-int bfs(int bidx,int tidx, vector<vector<int>> v){
+int bfs(int idx, vector<vector<int>> v){
+    
+    int len = v.size();
+    
     queue<pair<int,int>> q;
-    vector<int> dist(v.size(),INT_MAX);
-    q.push({bidx,0});
-    dist[bidx] = 0;
+    vector<int> dist(len,INT_MAX);
+    q.push({0,0});
+    dist[0]=0;
     
     while(!q.empty()){
-        int curIdx = q.front().first;
-        int curDist = q.front().second;
+        int cur = q.front().first;
+        int curd = q.front().second;
         q.pop();
-        if(tidx==curIdx){
-            return curDist;
+        if(cur==idx){
+            return dist[cur];
         }
-        for(int i=0;i<v[curIdx].size();i++){
-            int nextIdx = v[curIdx][i];
-            int nextDist = curDist+1;
-            if(nextDist>=dist[nextIdx]) continue;
-            dist[nextIdx] = nextDist;
-            q.push({nextIdx,nextDist});
-        }    
+        
+        for(int i=0;i<v[cur].size();i++){
+            int next = v[cur][i];
+            int nd = curd+1;
+            if(nd>=dist[next]) continue;
+            q.push({next,nd});
+            dist[next]=nd;
+        }
     }
-    
-    return dist[tidx];
+    return dist[idx];
 }
 
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
-    
-    int bidx = find(words.begin(),words.end(),begin) - words.begin();
-    int tidx = find(words.begin(),words.end(),target) - words.begin();
-
-    if(tidx==words.size()){
-        return 0;
-    }
-    
-    if(bidx==words.size()){
-        words.push_back(begin);
-    }
+    auto iter = find(words.begin(),words.end(),target);
+    if(iter==words.end()) return 0;
+    iter = find(words.begin(),words.end(),begin);
+    if(iter==words.end()) words.insert(words.begin(),begin);
     
     vector<vector<int>> v(words.size());
-    
     for(int i=0;i<words.size();i++){
-        for(int j=i+1;j<words.size();j++){
-            if(matching(words[i],words[j])){
+        for(int j=0;j<words.size();j++){
+            if(i==j) continue;
+            if(match(words[i],words[j])){
                 v[i].push_back(j);
                 v[j].push_back(i);
             }
+            
         }
     }
     
-    answer = bfs(bidx,tidx,v);
+    auto tidx = find(words.begin(),words.end(),target);
+    answer = bfs(tidx-words.begin(),v);
+    
+    
     
     return answer;
 }
