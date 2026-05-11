aaf9079a6bbb1d57f0c74517bceff0097aed216f
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-11 13:11:15 +0900

    [level 3] Title: 양과 늑대, Time: 0.27 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,41 +1,42 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 int answer = 0;
-
-void dfs(int n, int sheeps,int wolves,vector<bool> candidates,vector<int>& info,unordered_map<int,vector<int>>& graph )
-{
-    int size = candidates.size();
-    if(!info[n]) sheeps++;
-    else wolves++;
-    if(sheeps>0 && wolves>=sheeps) return;
-    answer = max(answer,sheeps);
-    for (auto& ele : graph[n]){
-        candidates[ele] = true;
-    }
-    for(int i=0;i<size;i++){
-        if(candidates[i]){
-            candidates[i] = false;
-            dfs(i,sheeps,wolves,candidates,info,graph);
-            candidates[i]= true;
+void bdfs(int sheep,int wolf,vector<int>& info, vector<vector<int>>& v,vector<bool>& candidate,vector<bool>& visited){
+    
+    if(wolf>=sheep) return;
+    answer= max(answer,sheep);
+    
+    for(int i=0;i<candidate.size();i++){
+        if(candidate[i]){
+            for(int j=0;j<v[i].size();j++){
+                int next = v[i][j];//1
+                if(visited[next]) continue;
+                candidate[next] = true;
+                visited[next] = true;
+                int sw = info[next];
+                if(sw==0) bdfs(sheep+1,wolf,info,v,candidate,visited);
+                else bdfs(sheep,wolf+1,info,v,candidate,visited);
+                visited[next] = false;
+                candidate[next] = false;
+            }
         }
     }
-    return;
-    
     
 }
 
-
 int solution(vector<int> info, vector<vector<int>> edges) {
     
-    vector<bool> candidates(info.size());
-    unordered_map<int,vector<int>> graph;
+    vector<vector<int>> v(info.size());
     for(auto edge:edges){
-        graph[edge[0]].push_back(edge[1]);
+        v[edge[0]].push_back(edge[1]);
     }
+    vector<bool> visited(info.size(),false);
+    vector<bool> candidate(info.size(),false);
+    candidate[0]=true;
+    visited[0] = true;
     
-    dfs(0,0,0,candidates,info,graph);
+    bdfs(1,0,info,v,candidate,visited);
     
     
     return answer;
