04e4b525d79f1ac5e44e2306d18cdb453bf3b70f
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-12 10:04:56 +0900

    [level 3] Title: 기지국 설치, Time: 0.09 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -1,34 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int cal(int len, int width){
-    int a = len/width;
-    int b = len%width;
-    if(b==0) return a;
-    else return a+1;
-}
-
 int solution(int n, vector<int> stations, int w)
 {
     int answer = 0;
-    //다음 것의 왼쪽이 이전것의 오른쪽보다 작다면 다음 오른쪽을 갱신하고 이런식으로
-    //크다면 그것의 길이를 그전까지의 길이를 계산하는거임 -> cal
-    
-    int left =0, right =0;
-    
+    int l = w*2+1;
+    int cur=1;
+    vector<int> v;
     for(auto station:stations){
-        station--;
-        right = station - w - 1;
-        if(left<=right){//겹쳐있지않다면
-            answer += cal(right-left+1,2*w+1);
+        int a = station-w-1;
+        int b = station+w+1;
+        int len = a-cur+1;
+        if(len>0) {
+            answer+=len/l;
+            if(len%l!=0)answer++;
         }
-        left = station + w +1;
+        cur=b;
     }
-    if(left<n){
-        right = n-1;
-        answer += cal(right-left+1,2*w+1);
+    
+    if(n-cur>=0){
+        answer+=(n-cur+1)/l;
+        if((n-cur+1)%l!=0) answer++;
     }
+ 
     
     return answer;
 }
