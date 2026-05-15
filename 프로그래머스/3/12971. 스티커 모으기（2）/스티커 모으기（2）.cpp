9e24675e6ce9a3401cf4c3fcc4780d345051662d
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-15 12:03:42 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.63 ms, Memory: 7.21 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -5,24 +5,19 @@ int solution(vector<int> sticker)
 {
     int answer =0;
     if(sticker.size()==1) return sticker[0];
+    vector<int> dp1(sticker.size()-1);
+    vector<int> dp2(sticker.size()-1);
     
-    vector<int> v1(sticker.begin(),sticker.end()-1);
-    vector<int> v2(sticker.begin()+1,sticker.end());
+    dp1[0] = sticker[0];
+    dp1[1] = max(sticker[0],sticker[1]);
+    dp2[0] = sticker[1];
+    dp2[1] = max(sticker[1],sticker[2]);
     
-    vector<int> dp1(v1.size(),0);
-    vector<int> dp2(v2.size(),0);
-    
-    dp1[0] = v1[0];
-    dp1[1] = max(v1[0],v1[1]);
-    
-    dp2[0] = v2[0];
-    dp2[1] = max(v2[0],v2[1]);
-    
-    int sz = v1.size();
-    for(int i=2;i<sz;i++){
-        dp1[i] = max(dp1[i-1],dp1[i-2]+v1[i]);
-        dp2[i] = max(dp2[i-1],dp2[i-2]+v2[i]);
+    for(int i=2;i<sticker.size()-1;i++){
+        dp1[i]= max(dp1[i-1],dp1[i-2]+sticker[i]);
+        dp2[i] = max(dp2[i-1],dp2[i-2]+sticker[i+1]);
     }
     
-    return max(dp1[dp1.size()-1],dp2[dp2.size()-1]);
+    
+    return max(dp1[sticker.size()-2],dp2[sticker.size()-2]);
 }
