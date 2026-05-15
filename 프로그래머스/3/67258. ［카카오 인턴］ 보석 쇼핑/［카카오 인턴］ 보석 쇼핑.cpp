eb0f9583296a0f91aa35976ae54f6c18735b8516
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-15 15:50:10 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 62.58 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -5,48 +5,38 @@ using namespace std;
 vector<int> solution(vector<string> gems) {
     vector<int> answer;
     map<string,int> m;
-    if(gems.size()==1) return {1,1};
-    int glen = gems.size();
-    int left =0;
-    int right =0;
-    int size=0;
-    
-    set<string> s;
-    for(int i=0;i<glen;i++){
-        s.insert(gems[i]);
+    for(auto gem:gems){
+        m[gem]++;
     }
-    int tsize= s.size(); // 보석 종류개수
-
-    int len = INT_MAX;
+    int gs = m.size();
+    
+    map<string,int> g;
+    int sz = 0;
+    int idx =0;
+    int ml = INT_MAX;
     int mleft =INT_MAX;
     int mright =INT_MAX;
-    for(int i=0;i<glen;i++){
-        
-        m[gems[i]]++;
-        if(m[gems[i]]==1) size++;
-        right =i;
-        
-        if(size==tsize){
-            while(left<=right){
-                m[gems[left]]--;
-                if(m[gems[left]]==0){
-                    size--;
-                    
-                    if(len>right-left+1){
-                        len = right-left+1;
-                        mleft=left;
-                        mright=right;
-                        
+    for(int i=0;i<gems.size();i++){
+        g[gems[i]]++;
+        if(g[gems[i]]==1) sz++;
+        if(sz==gs){
+            while(idx<=i){
+                g[gems[idx]]--;
+                if(g[gems[idx]]==0){
+                    sz--;
+                    if(ml > i-idx+1){
+                        ml=i-idx+1;
+                        mleft= idx+1;
+                        mright = i+1;
                     }
-                    left++;
+                    idx++;
                     break;
                 }
-                left++;
+                idx++;
             }
         }
-        
     }
     
     
-    return {mleft+1,mright+1};
+    return {mleft,mright};
 }
