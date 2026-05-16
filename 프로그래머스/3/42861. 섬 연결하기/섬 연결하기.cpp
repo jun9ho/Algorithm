2b4a4f059b202eba389db04afcd7004b18665a78
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-16 20:39:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
+    
     vector<vector<pair<int,int>>> v(n);
-    for(auto cost:costs){
+    for(auto cost: costs){
         v[cost[0]].push_back({cost[1],cost[2]});
         v[cost[1]].push_back({cost[0],cost[2]});
     }
     
-    vector<bool> vis(n, false);
-    //edge cost, node
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    
-    pq.push({0,0});
-    long long total = 0;
-    int count = 0;
-    while(!pq.empty() && count<n){
-        int w = pq.top().first; //cost
-        int u = pq.top().second; //node;
-        pq.pop();
-        if(vis[u]) continue;
-        vis[u]=true;
-        total+=w;
-        count++;
-        for(int i=0;i<v[u].size();i++){
-            int nu = v[u][i].first;
-            int nw = v[u][i].second;
-            if(!vis[nu]) pq.push({nw,nu});
-            
-        }
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    vector<bool> dist(n,false);
+    dist[0] = true;
+    for(int i=0;i<v[0].size();i++){
+        pq.push({v[0][i].second,v[0][i].first});
     }
     
-    return total;
+    while(!pq.empty()){
+        int curd = pq.top().first;
+        int cur = pq.top().second;
+        pq.pop();
+        if(dist[cur]) continue;
+        dist[cur] = true;
+        answer+=curd;
+        
+        for(int i=0;i<v[cur].size();i++){
+            pq.push({v[cur][i].second,v[cur][i].first});
+        }
+        
+    }
+    
+    
+    return answer;
 }
