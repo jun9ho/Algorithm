e3b7dd6da99a9d196e76a1a3389a0d2214043aae
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-16 20:52:46 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 5.26 ms, Memory: 28.6 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -3,28 +3,39 @@
 using namespace std;
 
 long long solution(vector<int> sequence) {
-    long long answer = 0;
+    long long answer = LONG_MIN;
+    vector<long long> dp1(sequence.size());
+    vector<long long> dp2(sequence.size());
     
-    vector<long long> dp1(sequence.size(),0); //1 -1
-    vector<long long> dp2(sequence.size(),0); //-1 1
-    //1. 부분수열의 최대값 구하는 방식을 알아야함
+    dp1[0] = (long long)sequence[0];
+    dp1[1] = max(dp1[0]+sequence[1]*-1, (long long)sequence[1]*-1);
     
-    int size = sequence.size();
-    dp1[0] = sequence[0];
-    dp2[0] = sequence[0] *-1;
-    for(int i=1;i<size;i++){
-        long long a = sequence[i];
-        long long b = sequence[i];
-        if(i%2==1) a*=-1;
-        else b*=-1;
-        dp1[i] = max(a+dp1[i-1],a);
-        dp2[i] = max(b+dp2[i-1],b);
+    dp2[0] = (long long)sequence[0]*-1;
+    dp2[1] = max(dp2[0]+(long long)sequence[1], (long long)sequence[1]);
+    
+    for(int i=2;i<sequence.size();i++){
+        if(i%2==0){
+            dp1[i] = max(dp1[i-1]+(long long)sequence[i], (long long)sequence[i]);
+            dp2[i] = max(dp2[i-1]+(long long)sequence[i]*-1, (long long)sequence[i]*-1);
+        }
+        else{
+            dp1[i] = max(dp1[i-1]+(long long)sequence[i]*-1, (long long)sequence[i]*-1);
+            dp2[i] = max(dp2[i-1]+(long long)sequence[i], (long long)sequence[i]);
+        }
+        
     }
-    long long maxx1= -1;
-    long long maxx2= -1;
-    for(int i=0;i<dp1.size();i++){
-        maxx1=max(dp1[i],maxx1);
-        maxx2=max(dp2[i],maxx2);
+    
+    //2 -3 -6 -1 3 1 2 -4
+    //2 -1 -6 -1 3 4 6 2
+    
+    //-2 3 6 1 -3 -1 -2 4
+    //-2 3 9 10 7 6 4 8
+    for(int i=0;i<sequence.size();i++){
+        answer = max(answer,dp1[i]);
+        answer = max(answer,dp2[i]);
+        
     }
-    return max(maxx1,maxx2);
+    
+    
+    return answer;
 }
