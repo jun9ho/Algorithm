8fd7cb10b72de4c8bf20a3d44fdeb9e315b4d7dc
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-17 11:00:06 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.23 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -1,57 +1,44 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using T=tuple<int,int,int,int>;
 
-// 직선 도로 100원, 코너 500원
-
-int dx[4] ={1,0,-1,0};
-int dy[4] ={0,1,0,-1};
-
-
-
+int dx[4]= {1,0,-1,0};
+int dy[4]= {0,1,0,-1};
+        //  0 1 2  3
 int bfs(vector<vector<int>> board){
     int n = board.size();
+    queue<tuple<int,int,int,int>> q;//x,y,money(dist), prev(0~3) 
     vector<vector<vector<int>>> dist(n,vector<vector<int>>(n,vector<int>(4,INT_MAX)));
-    priority_queue<T,vector<T>,greater<T>> q; //dist,pre, x, y
-    q.push({0,0,0,-1});
     for(int i=0;i<4;i++){
         dist[0][0][i]=0;
     }
+    q.push({0,0,0,-1});
     while(!q.empty()){
-        auto [curd,curx,cury,pre] = q.top();
+        auto [cx,cy,cd,prev] = q.front();
         q.pop();
-        if(curx==n-1 && cury==n-1) return curd;
-
         for(int i=0;i<4;i++){
-            int nx = curx+dx[i];
-            int ny = cury+dy[i];
-            int nd;
-            
-            if(nx>=n || ny>=n || nx<0 || ny<0) continue;
-            if(board[nx][ny]) continue;
-            
-            if(pre==-1)nd = curd+100;
-            else if (pre!=i) nd = curd+600;
-            else nd=curd+100;
-            
+            auto nx = cx+dx[i];
+            auto ny = cy+dy[i];
+            if(nx<0||ny<0||nx>=n||ny>=n) continue;
+            if(board[nx][ny])continue;
+            auto nd = cd;
+            if(prev==i||prev==-1) nd+=100;
+            else nd+=600;
             if(dist[nx][ny][i]<=nd) continue;
-            
             dist[nx][ny][i]= nd;
-            q.push({nd,nx,ny,i});
+            q.push({nx,ny,nd,i});
         }
-        
-        
     }
-    
-    return min(min(dist[n-1][n-1][0],dist[n-1][n-1][1]),min(dist[n-1][n-1][2],dist[n-1][n-1][3]));
+    int minn =INT_MAX;
+    for(int i=0;i<4;i++){
+        minn=min(minn,dist[n-1][n-1][i]);
+    }
+    return minn;
 }
 
 int solution(vector<vector<int>> board) {
     int answer = 0;
-    
     answer = bfs(board);
-    
     
     return answer;
 }
