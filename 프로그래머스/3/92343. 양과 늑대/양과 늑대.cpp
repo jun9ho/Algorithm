102cf49603ddb365073fffc15dac874a6df09cd0
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-18 16:20:24 +0900

    [level 3] Title: 양과 늑대, Time: 1.09 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,42 +1,40 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 int answer = 0;
-void bdfs(int sheep,int wolf,vector<int>& info, vector<vector<int>>& v,vector<bool>& candidate,vector<bool>& visited){
+void dfs(int n,vector<int>& info, vector<vector<int>>& v,int sheep,int wolf,vector<bool> candidate){
     
     if(wolf>=sheep) return;
-    answer= max(answer,sheep);
+    
+    answer = max(sheep,answer);
     
     for(int i=0;i<candidate.size();i++){
         if(candidate[i]){
             for(int j=0;j<v[i].size();j++){
-                int next = v[i][j];//1
-                if(visited[next]) continue;
-                candidate[next] = true;
-                visited[next] = true;
-                int sw = info[next];
-                if(sw==0) bdfs(sheep+1,wolf,info,v,candidate,visited);
-                else bdfs(sheep,wolf+1,info,v,candidate,visited);
-                visited[next] = false;
-                candidate[next] = false;
+                //v[i][j] 다음 노드
+                if(candidate[v[i][j]]) continue;
+                candidate[v[i][j]]=true;
+                if(info[v[i][j]]) dfs(n,info,v,sheep,wolf+1,candidate);
+                else dfs(n,info,v,sheep+1,wolf,candidate);
+                candidate[v[i][j]]=false;
             }
+            
         }
     }
-    
+    return;
 }
 
 int solution(vector<int> info, vector<vector<int>> edges) {
-    
-    vector<vector<int>> v(info.size());
+    int n = info.size();// 총 노드 수
+    vector<vector<int>> v(n);// 트리
     for(auto edge:edges){
         v[edge[0]].push_back(edge[1]);
     }
-    vector<bool> visited(info.size(),false);
-    vector<bool> candidate(info.size(),false);
+    vector<bool> candidate(n,false);
     candidate[0]=true;
-    visited[0] = true;
+    dfs(n,info,v,1,0,candidate);
     
-    bdfs(1,0,info,v,candidate,visited);
     
     
     return answer;
