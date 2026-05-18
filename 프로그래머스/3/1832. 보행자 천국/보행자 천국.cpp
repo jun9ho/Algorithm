18ec78ea53b89efeff5f6c2bc1e0b2d27eec5736
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-18 16:36:18 +0900

    [level 3] Title: 보행자 천국, Time: 24.86 ms, Memory: 7.42 MB -BaekjoonHub

---
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int N, M;
+int DP[510][510][2];
+
+int MOD = 20170805;
+int solution(int m, int n, vector<vector<int>> city_map){
+    N = m;
+    M = n;
+    memset(DP, 0, sizeof(DP));
+    DP[1][1][0] = DP[1][1][1] = 1;
+    for (int i = 1; i <= N; i++){        
+        for (int j = 1; j <= M; j++){        
+            if (city_map[i - 1][j - 1] == 0){              
+                DP[i][j][0] = (DP[i][j][0] + DP[i - 1][j][0] + DP[i][j - 1][1]) % MOD;    
+                DP[i][j][1] = (DP[i][j][1] + DP[i - 1][j][0] + DP[i][j - 1][1]) % MOD;            
+            }         
+            else if (city_map[i - 1][j - 1] == 1)
+                DP[i][j][0] = DP[i][j][1] = 0;          
+            else           
+            {           
+                DP[i][j][0] = DP[i - 1][j][0] % MOD; 
+                DP[i][j][1] = DP[i][j - 1][1] % MOD;      
+            }       
+        }   
+    }    
+    return DP[N][M][1] % MOD;
+}
+
+
+
