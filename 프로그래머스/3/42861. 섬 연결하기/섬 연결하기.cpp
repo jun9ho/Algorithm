64191538a35e7623e06752b847d0158e1fcf481f
Jung Ho Hwang <129595833+jun9ho@users.noreply.github.com>
2026-05-27 15:52:56 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -6,34 +6,30 @@ int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
     
     vector<vector<pair<int,int>>> v(n);
-    for(auto cost: costs){
+    vector<bool> vis(n,false);
+    
+    for(auto cost:costs){
         v[cost[0]].push_back({cost[1],cost[2]});
         v[cost[1]].push_back({cost[0],cost[2]});
     }
     
     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    vector<bool> vis(n,false);
-    vis[0] = true;
+    
     for(int i=0;i<v[0].size();i++){
         pq.push({v[0][i].second,v[0][i].first});
     }
-    
+    vis[0]=true;
     while(!pq.empty()){
-        int cd = pq.top().first;
-        int cur = pq.top().second;
+        int cur  = pq.top().second;
+        int curd = pq.top().first;
         pq.pop();
         if(vis[cur]) continue;
-        answer+=cd;
+        answer+=curd;
         vis[cur]=true;
         for(int i=0;i<v[cur].size();i++){
-            int next = v[cur][i].first;
-            int nd = v[cur][i].second;
-            pq.push({nd,next});
+            pq.push({v[cur][i].second,v[cur][i].first});
         }
-        
-        
     }
-    
     
     return answer;
 }
